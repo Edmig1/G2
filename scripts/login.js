f1bbd3e3a216af57ab6fde242be09ea84c70c145
Murilo M. <miguelitoplayer1@gmail.com>
2025-10-21 14:44:31 -0300

    login dando certo

--- scripts/login.js
+++ scripts/login.js
@@ -1,12 +1,44 @@
+// LOGIN____________________________________________________________
+let users = ["gabriel", "miguel"];
+let passwords = ["1010", "0909"];
 
-//LOGIN____________________________________________________________
+const esqueci = document.getElementsByClassName("esqueci")[0];
+const login = document.getElementById("logintext");
+const cardLog = document.getElementsByClassName("cardLog")[0];
+const submit = document.getElementById("submit");
 
-esqueci = document.getElementsByClassName("esqueci")[0]
-cardLog = document.getElementsByClassName("cardLog")[0]
-esqueci.addEventListener("click",(event)=>{
-    event.preventDefault()
-    cardLog.style.transform = 'translateX(100vw)'
-        setTimeout(() => {
-        window.location.href = "../front/cadastro.html"
+esqueci.addEventListener("click", (event) => {
+    event.preventDefault();
+    cardLog.style.transform = "translateX(100vw)";
+    setTimeout(() => {
+        window.location.href = "../front/cadastro.html";
     }, 1000);
-})
+});
+
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    let loginCerto = false;
+
+    for (let i = 0; i < passwords.length; i++) {
+        if (email === users[i] && senha === passwords[i]) {
+            loginCerto = true;
+            break;
+        }
+    }
+
+    if (loginCerto) {
+        console.log("certo");
+        window.location.href = "../front/index.html";
+    } else {
+        console.log("errado");
+        login.innerHTML = "Login ou senha errados";
+        login.style.color = "orange";
+        setTimeout(() => {
+            login.innerHTML = "Faça seu Login";
+            login.style.color = "white";
+        }, 2000);
+    }
+});
