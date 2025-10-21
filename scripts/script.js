73c1927630023f6de11d1ad4e17458f700d1353c
Murilo M. <miguelitoplayer1@gmail.com>
2025-10-21 14:10:59 -0300

    att

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@
 const menu = document.getElementById("menu")
 const nav = document.getElementById("navmob")
 const cont = document.getElementById("containerpop")
+const x = document.getElementById("x")
 
 menu.addEventListener("click", (event) => {
     event.preventDefault()
@@ -12,4 +13,12 @@ menu.addEventListener("click", (event) => {
         cont.style.opacity = 1
         nav.style.transform = 'translateX(0vw)'
     }, 50)
+})
+
+x.addEventListener("click",() =>{
+    cont.style.opacity = 0
+    nav.style.transform = 'translateX(-100vw)'
+        setTimeout(() => {
+            cont.style.display = 'none'
+    }, 1000)
 })
