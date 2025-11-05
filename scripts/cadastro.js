d5eb313d65f1d3e697ca7e00eb74ce906aa69eac
Murilo M. <miguelitoplayer1@gmail.com>
2025-11-05 14:15:58 -0300

    cadastro funcionando

---
+++ scripts/cadastro.js
@@ -0,0 +1,6 @@
+const login = document.getElementById("logintext");
+const cardLog = document.getElementsByClassName("cardLog")[0];
+const submit = document.getElementById("submit");
+
+const email = document.getElementById("emailcadastro").value;
+const senha = document.getElementById("senhacadastro").value;
--- scripts/login.js
+++ scripts/login.js
@@ -1,44 +1,53 @@
+//CADASTRO_____________________________________________
+
+const submitcadastro = document.getElementById("submitcadastro");
+
+if (submitcadastro) {
+    submitcadastro.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const emailcadastro = document.getElementById("emailcadastro").value;
+        const senhacadastro = document.getElementById("senhacadastro").value;
+
+        // Atualiza com dados mais recentes do localStorage
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+        let passwords = JSON.parse(localStorage.getItem("passwords")) || [];
+
+        users.push(emailcadastro);
+        passwords.push(senhacadastro);
+
+        localStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("passwords", JSON.stringify(passwords));
+
+        console.log("Usuário cadastrado com sucesso!");
+    });
+}
+
+
+
 // LOGIN____________________________________________________________
-let users = ["gabriel", "miguel"];
-let passwords = ["1010", "0909"];
 
 const esqueci = document.getElementsByClassName("esqueci")[0];
 const login = document.getElementById("logintext");
 const cardLog = document.getElementsByClassName("cardLog")[0];
 const submit = document.getElementById("submit");
 
-esqueci.addEventListener("click", (event) => {
-    event.preventDefault();
-    cardLog.style.transform = "translateX(100vw)";
-    setTimeout(() => {
-        window.location.href = "../front/cadastro.html";
-    }, 1000);
-});
 
-submit.addEventListener("click", (event) => {
-    event.preventDefault();
+if (submit) {
+    submit.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const senha = document.getElementById("senha").value;
-    let loginCerto = false;
+        const email = document.getElementById("email").value;
+        const senha = document.getElementById("senha").value;
 
-    for (let i = 0; i < passwords.length; i++) {
-        if (email === users[i] && senha === passwords[i]) {
-            loginCerto = true;
-            break;
+        let users = JSON.parse(localStorage.getItem("users"));
+        let passwords = JSON.parse(localStorage.getItem("passwords"));
+        for(i=0; i< users.length; i++){
+            if(users[i] ==email && passwords[i] == senha){
+                window.location.href = "../front/index.html";
+            }
         }
-    }
 
-    if (loginCerto) {
-        console.log("certo");
-        window.location.href = "../front/index.html";
-    } else {
-        console.log("errado");
-        login.innerHTML = "Login ou senha errados";
-        login.style.color = "orange";
-        setTimeout(() => {
-            login.innerHTML = "Faça seu Login";
-            login.style.color = "white";
-        }, 2000);
-    }
-});
+
+    });
+}
